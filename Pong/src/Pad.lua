e94295262b0656073d58a368369b2e92ddb4094d
A Puma on a Penguin <kikefontanlorenzo@gmail.com>
2018-07-12 00:03:20 +0200

    :zap: Pong: Pads

--- Pong/src/Pad.lua
+++ Pong/src/Pad.lua
@@ -1,24 +1,26 @@
 Pad = Object:extend()
 
-function Pad:new(x, w, h)
-  self.x = x
-  self.y = (love.graphics.getHeight() - h) / 2
-  self.w = w
-  self.h = h
+function Pad:new(align)
+  local padding = 20
+  self.w = 10
+  self.h = 70
+  self.x = align == "left" and self.w + padding or love.graphics.getWidth() - self.w * 2 - padding
+  self.y = (love.graphics.getHeight() - self.h) / 2
   self.speed = 100
+  self.keybind = align == "left" and { up = "w", down = "s" } or { up = "up", down = "down" }
 end
 
 function Pad:update(dt)
-  if love.keyboard.isDown("s") and self.y < love.graphics.getHeight() - self.h then
+  if love.keyboard.isDown(self.keybind.down) and self.y < love.graphics.getHeight() - self.h then
     self.y = self.y + self.speed * dt
   end
-  if love.keyboard.isDown("w") and self.y > 0 then
+  if love.keyboard.isDown(self.keybind.up) and self.y > 0 then
     self.y = self.y - self.speed * dt
   end
 end
 
 function Pad:draw()
-  love.graphics.rectangle("line", self.x, self.y, self.w, self.h)
+  love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
 end
 
 return Pad
